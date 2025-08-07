5c461a6c1fb0a759b4bcb60e848a441413755d61
vitaliy-bezugly <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2025-08-07 15:33:42 +0300

    feat(requests): ✨ Add data source attributes to LanguageRequest properties

--- Apps.MotionPoint/Models/Requests/LanguageRequest.cs
+++ Apps.MotionPoint/Models/Requests/LanguageRequest.cs
@@ -1,15 +1,17 @@
-﻿using Blackbird.Applications.Sdk.Common;
+﻿using Apps.MotionPoint.Handlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.MotionPoint.Models.Requests;
 
 public class LanguageRequest
 {
-    [Display("Source language")]
+    [Display("Source language"), DataSource(typeof(SourceLanguageDataHandler))]
     public string SourceLanguage { get; set; } = string.Empty;
     
-    [Display("Target language")]
+    [Display("Target language"), DataSource(typeof(TargetLanguageDataHandler))]
     public string TargetLanguage { get; set; } = string.Empty;
     
-    [Display("Country")]
+    [Display("Country"), DataSource(typeof(CountryDataHandler))]
     public string? Country { get; set; }
 }
